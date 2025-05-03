473bb9e7909f195b05513482e41eb5c6d406ffe9
GerardoMandujano <gmandujano60@gmail.com>
2025-05-03 13:24:10 -0600

    Iniciando Proyecto

---
+++ src/test/java/mx/com/gmr/app/Terapias/TerapiasApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.gmr.app.Terapias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerapiasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
